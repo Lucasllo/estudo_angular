5da129553728bf27673410f35909d20ba0587f4f
Lucas Lopes <lucaslopesoliveira@gmail.com>
2022-05-25 13:40:08 -0300

    melhoria no carrinho

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  itens= this.cart.pegarItens();
 
-  constructor() { }
+  constructor(private cart:CartService) { }
 
   ngOnInit(): void {
   }
